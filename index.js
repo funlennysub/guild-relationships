6217095dc6e69e10b339d5184e272b14e9cbc55b
Bowser65 <bowoser@weeb.services>
2020-05-28 01:48:24 +0200

    Fix context menu and make component viable

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
 const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
-const { getModuleByDisplayName, getModule, React } = require("powercord/webpack");
-const { open } = require('powercord/modal')
+const { getModule, React } = require('powercord/webpack');
+const { open } = require('powercord/modal');
 const { resolve } = require('path');
 
-const Modal = require('./components/SRsModal.jsx')
+const Modal = require('./components/SRsModal.jsx');
 
 module.exports = class RelationShips extends Plugin {
+  async startPlugin () {
+    this._injectContextMenu();
+    this.loadCSS(resolve(__dirname, 'style.css'));
+  }
 
-    async startPlugin() {
-        this._injectContextMenu();  
-        this.loadCSS(resolve(__dirname, 'css.scss'))
-    }
-
-    async _injectContextMenu() {
-        const menu = await getModule(["MenuItem"])
-        const mdl = await getModule(m => m.default && m.default.displayName === 'GuildContextMenu');
-        inject('guild-relationships', mdl, 'default', ([{
-          target
-        }], res) => {
-          if (target.tagName.toLowerCase() === 'a') {
-            res.props.children.splice(4, 0,
-              React.createElement(menu.MenuItem, {
-                id: "guild-relationships",
-                label: "Guild Relationships",
-                action: () => open(() => React.createElement(Modal, { })),
-              })
-            );
-          }
-          return res;
-        });
-        mdl.default.displayName = 'GuildContextMenu';
+  async _injectContextMenu () {
+    const menu = await getModule([ 'MenuItem' ]);
+    const mdl = await getModule(m => m.default && m.default.displayName === 'GuildContextMenu');
+    inject('guild-relationships', mdl, 'default', ([ { target, guild: { id: guildId } } ], res) => {
+      if (target.tagName.toLowerCase() === 'a') {
+        res.props.children.splice(4, 0,
+          React.createElement(menu.MenuItem, {
+            id: 'guild-relationships',
+            label: 'Guild Relationships',
+            action: () => open(() => React.createElement(Modal, { guildId }))
+          })
+        );
       }
+      return res;
+    });
+    mdl.default.displayName = 'GuildContextMenu';
+  }
 
-      pluginWillUnload() {
-        uninject('guild-relationships')
-      }
-
-}
+  pluginWillUnload () {
+    uninject('guild-relationships');
+  }
+};
