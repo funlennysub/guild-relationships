ec6200a4a85e9f5ff930ca9de9f0e4463c33fa8e
funlennysub <kil.oil@yandex.ru>
2020-05-28 00:48:35 +0300

    Are you happy now, bowser?

--- components/SRsModal.jsx
+++ components/SRsModal.jsx
@@ -2,92 +2,96 @@ const { getModuleByDisplayName, React, getModule } = require('powercord/webpack'
 const { Category } = require('powercord/components/settings');
 const { AsyncComponent } = require('powercord/components');
 const FormTitle = AsyncComponent.from(getModuleByDisplayName('FormTitle'));
+const { Modal } = require('powercord/components/modal');
 
-module.exports = class ListOfUsers extends React.Component {
+const { useState, useEffect } = React
 
-constructor (props) {
-  super(props);
+module.exports = ({ }) => {
 
-  this.state = {
-      settingsFirendsOpened: false,
-      settingsBlockedOpened: false,
-      friends: [],
-      blockedUsers: []
-  };
-}
 
-async componentDidMount() {
+  const [friendsCategoryOpened, openFriends] = useState(false)
+  const [blockedCategoryOpened, openBlocked] = useState(false)
+  const [friends, setFriends] = useState([])
+  const [blockedUsers, setBlocked] = useState([])
 
-  const getUser = (await getModule(["getUser", "setFlag"])).getUser
-  const getMemberIds = (await getModule(["getMemberIds"])).getMemberIds
-  const isFriend = (await getModule(["isFriend"])).isFriend
-  const isBlocked = (await getModule(["isFriend", "isBlocked"])).isBlocked
+  useEffect(() => {
+    async function funcName() {
 
-  const guild = getModule(['getGuildId'], false).getGuildId();
+      const getUser = (await getModule(["getUser", "setFlag"])).getUser
+      const getMemberIds = (await getModule(["getMemberIds"])).getMemberIds
+      const isFriend = (await getModule(["isFriend"])).isFriend
+      const isBlocked = (await getModule(["isFriend", "isBlocked"])).isBlocked
 
-  let friendUsernames = []
-		getMemberIds(guild).forEach(id => {
-			if (isFriend(id)) {
-				friendUsernames.push(
-					getUser(id).then((user) => {
-            return { friendID: `${user.id}`, friendUsername: `${user.username}#${user.discriminator}`, userAvatarURL: `${user.avatarURL}` }
-          })
-        )
-			}
-    });
-  friendUsernames = await Promise.all(friendUsernames)
+      const guild = getModule(['getGuildId'], false).getGuildId();
 
-  const friend_arr = [];
-    for (const friend of friendUsernames) {
-      friend_arr.push(<div className="user-slDf"><div style={{backgroundImage: `url('${friend.userAvatarURL}')`}} className="userPfp"></div><span className="usename-jdkd">{friend.friendUsername}</span></div>);
+      let friendUsernames = []
+					getMemberIds(guild).forEach(id => {
+						if (isFriend(id)) {
+							friendUsernames.push(
+								getUser(id).then((user) => {
+                 return { friendID: `${user.id}`, friendUsername: `${user.username}#${user.discriminator}`, userAvatarURL: `${user.avatarURL}` }
+                })
+							)
+						}
+          });
+        friendUsernames = await Promise.all(friendUsernames)
+
+        const friend_arr = [];
+        for (const friend of friendUsernames) {
+          friend_arr.push(<div className="user-slDf"><div style={{backgroundImage: `url('${friend.userAvatarURL}')`}} className="userPfp"></div><span className="usename-jdkd">{friend.friendUsername}</span></div>);
+        }
+        setFriends(friend_arr)
+
+        let blockedUsernames = []
+					getMemberIds(guild).forEach(id => {
+						if (isBlocked(id)) {
+							blockedUsernames.push(
+								getUser(id).then((b_user) => {
+                  return { blockedID: `${b_user.id}`, blockedUsername: `${b_user.username}#${b_user.discriminator}`, userAvatarURL: `${b_user.avatarURL}` }
+                })
+							)
+						}
+          });    
+
+        blockedUsernames = await Promise.all(blockedUsernames)
+
+        const blocked_arr = [];
+        for (const blocked of blockedUsernames) {
+          blocked_arr.push(<div className="user-slDf"><div style={{backgroundImage: `url('${blocked.userAvatarURL}')`}} className="userPfp"></div><span className="usename-jdkd">{blocked.blockedUsername}</span></div>);
+        }
+        setBlocked(blocked_arr);
     }
-  this.setState({ friends: friend_arr })
+    funcName()
+  }, [])
 
-  let blockedUsernames = []
-    getMemberIds(guild).forEach(id => {
-			if (isBlocked(id)) {
-				blockedUsernames.push(
-					getUser(id).then((b_user) => {
-            return { blockedID: `${b_user.id}`, blockedUsername: `${b_user.username}#${b_user.discriminator}`, userAvatarURL: `${b_user.avatarURL}` }
-          })
-				)
-			}
-    });    
-
-  blockedUsernames = await Promise.all(blockedUsernames)
-
-  const blocked_arr = [];
-    for (const blocked of blockedUsernames) {
-      blocked_arr.push(<div className="user-slDf"><div style={{backgroundImage: `url('${blocked.userAvatarURL}')`}} className="userPfp"></div><span className="usename-jdkd">{blocked.blockedUsername}</span></div>);
-    }
-  this.setState({ blockedUsers: blocked_arr });
-}
-
-  render() {
 
     return (
-      <div className="sizeMedium-df47zS">
-        <FormTitle tag="h4" className="formTitle-LdsDK">Guild Relationships</FormTitle>
-        <Category
-        name="Friends"
-        opened={this.state.settingsFirendsOpened}
-        onChange={() => this.setState({ settingsFirendsOpened: !this.state.settingsFirendsOpened })}
-        >
-        <ul className="powercord-text">
-          {this.state.friends}
-        </ul>
-        </Category>
-        <Category
-        name="Blocked"
-        opened={this.state.settingsBlockedOpened}
-        onChange={() => this.setState({ settingsBlockedOpened: !this.state.settingsBlockedOpened })}
-        >
-        <ul className="powercord-text">
-          {this.state.blockedUsers}
-        </ul>
-        </Category>
-      </div>
+      <Modal className='guild-relationships-modal'>
+        <Modal.Header>
+          <FormTitle tag='h4'>Guild Relationships</FormTitle>
+        </Modal.Header>
+        <Modal.Content>
+          <Category
+          name="Friends"
+          opened={friendsCategoryOpened}
+          onChange={() => openFriends(!friendsCategoryOpened)}
+          >
+            <ul className="powercord-text">
+              {friends}
+            </ul>
+          </Category>
+          <Category
+          name="Blocked"
+          opened={blockedCategoryOpened}
+          onChange={() => openBlocked(!blockedCategoryOpened)}
+          >
+            <ul className="powercord-text">
+              {blockedUsers}
+            </ul>
+          </Category>
+        </Modal.Content>
+      </Modal>
     )
-  }
+
 
 }
